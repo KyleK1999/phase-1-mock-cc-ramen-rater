11124d01ae0860934aafcdd444d6810c09797367
Kyle <kylekibbe99@gmail.com>
2023-06-22 12:00:26 -0700

    deliverable 2

--- notes.md
+++ notes.md
@@ -1,6 +1,11 @@
 Deliverable 1 -
 See all ramen images in the div with the id of ramen-menu. When the page loads, request the data from the server to get all the ramen objects. Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
-
+[ ] fetch ramen
+[ ] iterate over
+[ ] creat an img for each
+[ ] add src
+[ ] select anchor point in nav
+[ ] append each img to anchor
 
 
 
@@ -13,3 +18,18 @@ Click on an image from the #ramen-menu div and see all the info about that ramen
 
 Deliverable 3 -
 Create a new ramen after submitting the new-ramen form. The new ramen should be added to the#ramen-menu div. The new ramen does not need to persist; in other words, if you refresh the page, it's okay that the new ramen is no longer on the page.
+
+
+
+data shape
+
+  "ramens": [
+    {
+      "id": 1,
+      "name": "Shoyu Ramen",
+      "restaurant": "Nonono",
+      "image": "./assets/ramen/shoyu.jpg",
+      "rating": 7,
+      "comment": "Delish. Can't go wrong with a classic!"
+    },
+  ]
--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 // write your code here
+//Globals
+const URL = "http://localhost:3000/ramens"
+
+
+
+//Fetches
+function getAllRamen(url) {
+    return fetch(url)
+          .then(response => response.json())
+}
+
+
+//Render Functions
+function renderInNav(ramenObj){
+    console.log('ramenObj:' , ramenObj)
+    const img = document.createElement('img')
+    img.src = ramenObj.image
+    img.addEventListener('click', () => renderDetail(ramenObj))
+    nav.appendChild(img)
+}
+//new ramen form
+const newRamenForm = document.getElementById('new-ramen')
+const ramenMenu = document.getElementById('ramen-menu')
+
+
+const nav = document.querySelector('#ramen-menu')
+const detailImage = document.querySelector(".detail-image")
+//
+const ramenName = document.querySelector(".name")
+const restaurant = document.querySelector(".restaurant")
+const rating = document.getElementById('rating-display')
+const comment = document.getElementById('comment-display')
+const ramenImage = document.querySelector('ramen-image')
+
+
+function renderDetail(ramenObj){
+    detailImage.src = ramenObj.image
+    ramenName.textContent = ramenObj.name
+    restaurant.textContent = ramenObj.restaurant
+    rating.textContent = ramenObj.rating
+    comment.textContent = ramenObj.comment
+} 
+
+
+
+
+//Event Handlers & Listeners
+
+
+
+
+//Initializers
+getAllRamen(URL).then(ramenArr => {
+    ramenArr.forEach(renderInNav)
+    renderDetail(ramenArr[0])
+})
